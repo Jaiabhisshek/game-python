96bbebf05f72c24cde668510932571f2f3690047
Jaiabhisshek8 <jaiabhissheks@gmail.com>
2023-12-30 16:30:23 +0530

    DONE

---
+++ game.py
@@ -0,0 +1,47 @@
+import random
+
+def get_player_choice():
+    while True:
+        player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+        if player_choice in ["rock", "paper", "scissors"]:
+            return player_choice
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.")
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (player_choice == "rock" and computer_choice == "scissors")
+        or (player_choice == "paper" and computer_choice == "rock")
+        or (player_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Welcome to Rock-Paper-Scissors Game!")
+
+    while True:
+
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+
+
+        print(f"You chose: {player_choice}")
+        print(f"Computer chose: {computer_choice}")
+
+        result = determine_winner(player_choice, computer_choice)
+        print(result)
+
+  
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing. Goodbye!")
+            break
+
+play_game()
